bbfe7f105675a87a891d2417899ce4689b674c68
Kevin Luque Gutierrez <96495440+Kevin1lg@users.noreply.github.com>
2024-11-15 11:05:45 -0500

    Update setup.py

--- src/actions_py/setup.py
+++ src/actions_py/setup.py
@@ -20,16 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "count_until_server = actions_py.count_until_server:main",
-            "count_until_server1 = actions_py.count_until_server1:main",
-            "count_until_server2 = actions_py.count_until_server2:main",
-            "count_until_server3 = actions_py.count_until_server3:main",
-            "count_until_server4 = actions_py.count_until_server4:main",
             "count_until_server5 = actions_py.count_until_server5:main",
-            "count_until_client = actions_py.count_until_client:main",
-            "count_until_client1 = actions_py.count_until_client1:main",
-            "count_until_client2 = actions_py.count_until_client2:main",
-            "count_until_client3 = actions_py.count_until_client3:main",
             "count_until_client4 = actions_py.count_until_client4:main"
         ],
     },
